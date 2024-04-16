e779fc03c1a74bdf5bcae95d2e9eab80e80d932a
Balar05 <arnaubalasch0506@gmail.com>
2024-04-16 19:12:25 +0200

    Started adding levels

--- Vampire_Killer/Source/TileMap.cpp
+++ Vampire_Killer/Source/TileMap.cpp
@@ -27,6 +27,7 @@ void TileMap::InitTileDictionary()
 	dict_rect[(int)Tile::BLOCK_LEFT] = { 0,  0, n, n };
 	dict_rect[(int)Tile::BLOCK_RIGHT] = { n,  0, n, n };
 	dict_rect[(int)Tile::BLOCK] = { 2 * n,  0, n, n };
+	dict_rect[(int)Tile::GRASS_BLOCK] = { 22 * n, 0, n, n };
 
 	dict_rect[(int)Tile::STAIRS_LEFT] = { 0, n, n, n };
 	dict_rect[(int)Tile::STAIRS_RIGHT] = { n, n, n, n };
@@ -82,7 +83,7 @@ AppStatus TileMap::Initialise()
 {
 	ResourceManager& data = ResourceManager::Instance();
 
-	if (data.LoadTexture(Resource::IMG_TILES, "images/Tilesets.png") != AppStatus::OK)
+	if (data.LoadTexture(Resource::IMG_TILES, "images/Tileset1.png") != AppStatus::OK)
 	{
 		return AppStatus::ERROR;
 	}
@@ -125,20 +126,20 @@ bool TileMap::IsTileSolid(Tile tile) const
 {
 	return (Tile::SOLID_FIRST <= tile && tile <= Tile::SOLID_LAST);
 }
-bool TileMap::IsTileLadderTop(Tile tile) const
-{
-	return tile == Tile::LADDER_TOP_L || tile == Tile::LADDER_TOP_R;
-}
-bool TileMap::IsTileLadder(Tile tile) const
-{
-	return tile == Tile::LADDER_L || tile == Tile::LADDER_R;
-}
+//bool TileMap::IsTileLadderTop(Tile tile) const
+//{
+//	return tile == Tile::LADDER_TOP_L || tile == Tile::LADDER_TOP_R;
+//}
+//bool TileMap::IsTileLadder(Tile tile) const
+//{
+//	return tile == Tile::LADDER_L || tile == Tile::LADDER_R;
 bool TileMap::TestCollisionWallLeft(const AABB& box) const
 {
 	return CollisionX(box.pos, box.height);
 }
 bool TileMap::TestCollisionWallRight(const AABB& box) const
 {
+//}
 	return CollisionX(box.pos + Point(box.width - 1, 0), box.height);
 }
 bool TileMap::TestCollisionGround(const AABB& box, int* py) const
@@ -193,79 +194,79 @@ bool TileMap::CollisionY(const Point& p, int distance) const
 		tile = GetTileIndex(x, y);
 
 		//One solid or laddertop tile is sufficient
-		if (IsTileSolid(tile) || IsTileLadderTop(tile))
+		if (IsTileSolid(tile) /* || IsTileLadderTop(tile)*/ )
 			return true;
 	}
 	return false;
 }
-bool TileMap::TestOnLadder(const AABB& box, int* px) const
-{
-	int left, right, bottom;
-	int tx1, tx2, ty;
-	Tile tile1, tile2;
-
-	//Control points
-	left = box.pos.x;
-	right = box.pos.x + box.width - 1;
-	bottom = box.pos.y + box.height - 1;
-
-	//Calculate the tile coordinates
-	tx1 = left / TILE_SIZE;
-	tx2 = right / TILE_SIZE;
-	ty = bottom / TILE_SIZE;
-
-	//To be able to climb up or down, both control points must be on ladder
-	tile1 = GetTileIndex(tx1, ty);
-	tile2 = GetTileIndex(tx2, ty);
-	if (IsTileLadder(tile1) && IsTileLadder(tile2))
-	{
-		*px = GetLadderCenterPos(left, bottom) - box.width / 2;
-		return true;
-	}
-	return false;
-}
-bool TileMap::TestOnLadderTop(const AABB& box, int* px) const
-{
-	int left, right, bottom;
-	int tx1, tx2, ty;
-	Tile tile1, tile2;
-
-	//Control points
-	left = box.pos.x;
-	right = box.pos.x + box.width - 1;
-	bottom = box.pos.y + box.height - 1;
-
-	//Calculate the tile coordinates
-	tx1 = left / TILE_SIZE;
-	tx2 = right / TILE_SIZE;
-	ty = bottom / TILE_SIZE;
-
-	//To be able to climb up or down, both control points must be on ladder
-	tile1 = GetTileIndex(tx1, ty);
-	tile2 = GetTileIndex(tx2, ty);
-	if (IsTileLadderTop(tile1) && IsTileLadderTop(tile2))
-	{
-		*px = GetLadderCenterPos(left, bottom) - box.width / 2;
-		return true;
-	}
-	return false;
-}
-int TileMap::GetLadderCenterPos(int pixel_x, int pixel_y) const
-{
-	int tx, ty;
-
-	tx = pixel_x / TILE_SIZE;
-	ty = pixel_y / TILE_SIZE;
-	Tile tile = GetTileIndex(tx, ty);
-
-	if (tile == Tile::LADDER_L || tile == Tile::LADDER_TOP_L)		return tx * TILE_SIZE + TILE_SIZE;
-	else if (tile == Tile::LADDER_R || tile == Tile::LADDER_TOP_R)	return tx * TILE_SIZE;
-	else
-	{
-		LOG("Internal error, tile should be a LADDER, coord: (%d,%d), tile type: %d", pixel_x, pixel_y, (int)tile);
-		return 0;
-	}
-}
+//bool TileMap::TestOnLadder(const AABB& box, int* px) const
+//{
+//	int left, right, bottom;
+//	int tx1, tx2, ty;
+//	Tile tile1, tile2;
+//
+//	//Control points
+//	left = box.pos.x;
+//	right = box.pos.x + box.width - 1;
+//	bottom = box.pos.y + box.height - 1;
+//
+//	//Calculate the tile coordinates
+//	tx1 = left / TILE_SIZE;
+//	tx2 = right / TILE_SIZE;
+//	ty = bottom / TILE_SIZE;
+//
+//	//To be able to climb up or down, both control points must be on ladder
+//	tile1 = GetTileIndex(tx1, ty);
+//	tile2 = GetTileIndex(tx2, ty);
+//	if (IsTileLadder(tile1) && IsTileLadder(tile2))
+//	{
+//		*px = GetLadderCenterPos(left, bottom) - box.width / 2;
+//		return true;
+//	}
+//	return false;
+//}
+//bool TileMap::TestOnLadderTop(const AABB& box, int* px) const
+//{
+//	int left, right, bottom;
+//	int tx1, tx2, ty;
+//	Tile tile1, tile2;
+//
+//	//Control points
+//	left = box.pos.x;
+//	right = box.pos.x + box.width - 1;
+//	bottom = box.pos.y + box.height - 1;
+//
+//	//Calculate the tile coordinates
+//	tx1 = left / TILE_SIZE;
+//	tx2 = right / TILE_SIZE;
+//	ty = bottom / TILE_SIZE;
+//
+//	//To be able to climb up or down, both control points must be on ladder
+//	tile1 = GetTileIndex(tx1, ty);
+//	tile2 = GetTileIndex(tx2, ty);
+//	if (IsTileLadderTop(tile1) && IsTileLadderTop(tile2))
+//	{
+//		*px = GetLadderCenterPos(left, bottom) - box.width / 2;
+//		return true;
+//	}
+//	return false;
+//}
+////int TileMap::GetLadderCenterPos(int pixel_x, int pixel_y) const
+////{
+////	int tx, ty;
+////
+////	tx = pixel_x / TILE_SIZE;
+////	ty = pixel_y / TILE_SIZE;
+////	Tile tile = GetTileIndex(tx, ty);
+////
+////	if (tile == Tile::LADDER_L || tile == Tile::LADDER_TOP_L)		return tx * TILE_SIZE + TILE_SIZE;
+////	else if (tile == Tile::LADDER_R || tile == Tile::LADDER_TOP_R)	return tx * TILE_SIZE;
+////	else
+////	{
+////		LOG("Internal error, tile should be a LADDER, coord: (%d,%d), tile type: %d", pixel_x, pixel_y, (int)tile);
+////		return 0;
+////	}
+////}
 void TileMap::Render()
 {
 	Tile tile;
--- Vampire_Killer/Source/TileMap.h
+++ Vampire_Killer/Source/TileMap.h
@@ -17,53 +17,42 @@ enum class Tile {
 
 	//Falta DOOR!
 
-	BLOCK = 1, BLOCK_LEFT = 2, BLOCK_RIGHT = 3, STAIRS_LEFT = 4, STAIRS_RIGHT = 5,
-	HEART_SMALL = 10, HEART_BIG = 11, HEART_PLAIN = 12,
+	BLOCK = 1, BLOCK_LEFT = 2, BLOCK_RIGHT = 3, STAIRS_LEFT = 4, STAIRS_RIGHT = 5, GRASS_BLOCK = 45,
 	CHEST = 6,
-	CANDLE_BIG = 7, CANDLE_SMALL = 8, FIRE_1 = 13, FIRE_2 = 14,
+
+	STAIRS_BOSS = 60,
+	
 	WHIP = 15, AXE = 16, KNIFE = 17,
 	BLUE_CROSS = 18, SILVER_CROSS = 19, GOLDEN_CROSS = 20,
 	SMALL_KEY = 21, BIG_KEY = 22,
 	WHITE_MONEY = 23, BLUE_MONEY = 24,
 	HOURGLASS = 25, HOLY_WATER = 26, STAFF = 27,
-	BOSS_ITEM1 = 28, BOSS_ITEM2 = 29, BOSS_ITEM3 = 30,
 	BLUE_ORB = 31,
 	BLUE_WATER = 32,
 	BOOTS = 33, RED_SHIELD = 34, GOLDEN_SHIELD = 35,
 	BLUE_RING = 36,
-	OBJECT_ANIM1 = 37, OBJECT_ANIM2 = 38, OBJECT_ANIM3 = 39,
 	BLACK_BIBLE = 40, WHITE_BIBLE = 41,
 	WINGS = 42,
 	PARCHMENT = 43,
+	
+
+	HEART_SMALL = 10, HEART_BIG = 11, HEART_PLAIN = 12,
+	CANDLE_BIG = 7, CANDLE_SMALL = 8, FIRE_1 = 13, FIRE_2 = 14,
+	BOSS_ITEM1 = 28, BOSS_ITEM2 = 29, BOSS_ITEM3 = 30,
+	OBJECT_ANIM1 = 37, OBJECT_ANIM2 = 38, OBJECT_ANIM3 = 39,
 	BOSS_ATTACK = 44,
-	STAIRS_BOSS = 60,
-
-
-	/*BLOCK_SQUARE1_TL = 1, BLOCK_SQUARE1_TR, BLOCK_SQUARE1_BL, BLOCK_SQUARE1_BR,
-	BLOCK_SQUARE2_TL, BLOCK_SQUARE2_TR, BLOCK_SQUARE2_BL, BLOCK_SQUARE2_BR,
-	BLOCK_VERT2_T, BLOCK_VERT2_B, BLOCK_HORIZ2_L, BLOCK_HORIZ2_R, BLOCK_BLUE,
-	BLOCK_HORIZ3_L, BLOCK_HORIZ3_M, BLOCK_HORIZ3_R,
-	BLOCK_BEAM_L, BLOCK_BEAM_R,*/
-	/*LADDER_L = 20, LADDER_R, LADDER_TOP_L, LADDER_TOP_R,
-	LOCK_RED = 30, LOCK_YELLOW,
-	LASER_L = 40, LASER_R,*/
-
-	// 50 <= id < 100: special tiles
-	/*DOOR = 50,
-	KEY_RED = 60, YELLOW_KEY, ITEM_APPLE, ITEM_CHILI,
-	LASER = 70, LASER_FRAME0, LASER_FRAME1, LASER_FRAME2,*/
 
 	// id >= 100: entities' initial locations
 	PLAYER = 100,
 
 	//Intervals
 
-	/*STATIC_FIRST = BLOCK_SQUARE1_TL,
-	STATIC_LAST = LASER_R,
-	SOLID_FIRST = BLOCK_SQUARE1_TL,
-	SOLID_LAST = BLOCK_BEAM_R,
-	SPECIAL_FIRST = DOOR,
-	SPECIAL_LAST = LASER,*/
+	STATIC_FIRST = BLOCK,
+	STATIC_LAST = CHEST,
+	SOLID_FIRST = BLOCK,
+	SOLID_LAST = STAIRS_BOSS,
+	SPECIAL_FIRST = WHIP,
+	SPECIAL_LAST = BOSS_ATTACK,
 
 	ENTITY_FIRST = PLAYER,
 	ENTITY_LAST = PLAYER
@@ -94,21 +83,21 @@ public:
 	bool TestFalling(const AABB& box) const;
 
 	//Test if box is on ladder and update 'px' with the x-center position of the ladder
-	bool TestOnLadder(const AABB& box, int* px) const;
+	// TestOnLadder(const AABB& box, int* px) const;
 
 	//Test if box is on ladder top and update 'px' with the x-center position of the ladder
-	bool TestOnLadderTop(const AABB& box, int* px) const;
+	//bool TestOnLadderTop(const AABB& box, int* px) const;
 
 private:
 	void InitTileDictionary();
 
 	Tile GetTileIndex(int x, int y) const;
 	bool IsTileSolid(Tile tile) const;
-	bool IsTileLadderTop(Tile tile) const;
-	bool IsTileLadder(Tile tile) const;
+	//bool IsTileLadderTop(Tile tile) const;
+	//bool IsTileLadder(Tile tile) const;
 	bool CollisionX(const Point& p, int distance) const;
 	bool CollisionY(const Point& p, int distance) const;
-	int GetLadderCenterPos(int pixel_x, int pixel_y) const;
+	//int GetLadderCenterPos(int pixel_x, int pixel_y) const;
 
 	//Tile map
 	Tile* map;
